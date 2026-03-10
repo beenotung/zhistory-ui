6cd3db10919032f39a06b5a90fde4fe8e68bbaaa
Beeno Tung <aabbcc1241@yahoo.com.hk>
2026-03-11 01:47:20 +0800

    feat: add webui (as a cli that write and open html file)

---
+++ src/web-ui.ts
@@ -0,0 +1,65 @@
+import { join } from 'path'
+import { formatTime, parseZHistory, readZHistory } from './core'
+import { tmpdir } from 'os'
+import { createWriteStream } from 'fs'
+import open from 'open'
+
+let text = readZHistory()
+let seq = parseZHistory(text)
+
+let file = join(tmpdir(), 'zhistory-webui.html')
+let stream = createWriteStream(file)
+
+stream.write(/* html */ `<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>ZHistory</title>
+<style>
+	body {
+	}
+	.container {
+		border-collapse: collapse;
+		max-width: 100%;
+		overflow-x: auto;
+	}
+	.line {
+	}
+	.time {
+		background-color: bisque;
+		padding: 4px;
+	}
+	.elapsed {
+		background-color: lightgreen;
+		padding: 4px;
+	}
+	.command {
+		white-space: pre-wrap;
+		margin: 1rem;
+		margin-top: 0.5rem;
+	}
+</style>
+</head>
+<body>
+<h1>ZHistory</h1>
+<div class="container">
+    `)
+for (let line of seq) {
+  stream.write(/* html */ `<div class="line">
+	<span class="time">${formatTime(line.startTime)}</span>
+	<span class="elapsed">${line.elapsedSeconds}</span>
+	<blockquote class="command">${line.command}</blockquote>
+</div>`)
+}
+
+stream.write(/* html */ `
+		</tbody>
+	</table>
+</div>
+</body>
+</html>`)
+
+stream.end()
+
+open(file)
