830a92acbd7cc69d75ffa5a7a04d626aa5a0a86e
Beeno Tung <aabbcc1241@yahoo.com.hk>
2026-03-11 02:27:48 +0800

    chore: setup export map for shorter export path

---
+++ examples/src/index.ts
@@ -0,0 +1,14 @@
+import * as fs from 'zhistory-ui/fs'
+import * as core from 'zhistory-ui'
+
+console.log({ fs, core })
+/*
+{
+  fs: { readZHistory: [Function: readZHistory] },
+  core: {
+    parseZHistory: [GeneratorFunction: parseZHistory],
+    parseLine: [Function: parseLine],
+    formatTime: [Function: formatTime]
+  }
+}
+*/
