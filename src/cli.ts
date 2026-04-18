e1ca2db751ca07dc57e0f4556f0492204feb5e9b
Beeno Tung <aabbcc1241@yahoo.com.hk>
2026-04-18 22:55:57 +0800

    feat: allow searching from zhistory using pattern matching

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,23 @@
 import { formatTime, parseZHistory } from './core'
 import { readZHistory } from './fs'
 
+let patterns = process.argv.slice(2)
+
 let text = readZHistory()
 let seq = parseZHistory(text)
-for (let line of seq) {
-  console.log(formatTime(line.startTime), line.elapsedSeconds, line.command)
+if (patterns.length === 0) {
+  for (let line of seq) {
+    console.log(formatTime(line.startTime), line.elapsedSeconds, line.command)
+  }
+} else {
+  let seen_commands = new Set<string>()
+  let self_pattern = patterns.map(pattern => ' ' + pattern).join('')
+  for (let line of seq) {
+    let matched = patterns.every(pattern => line.command.includes(pattern))
+    if (!matched) continue
+    if (line.command.endsWith(self_pattern)) continue
+    if (seen_commands.has(line.command)) continue
+    seen_commands.add(line.command)
+    console.log(formatTime(line.startTime), line.elapsedSeconds, line.command)
+  }
 }
